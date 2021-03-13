99453c28a2aee56bf322463124b2d244cf401b19
mander321 <cristian4448@gmail.com>
2021-03-13 17:01:10 -0300

    autocomplete v2

--- client/src/components/SearchBar/index.tsx
+++ client/src/components/SearchBar/index.tsx
@@ -1,30 +1,26 @@
 import React, { useState } from "react";
 import { StyledSearchBar } from "./StyledSearchBar";
 import { SEARCH_PRODUCTS, GET_PRODUCTS } from "../../graphql/queries";
-import { useQuery } from "@apollo/react-hooks";
+import { useLazyQuery } from "@apollo/react-hooks";
 import { QueryProducts } from "./../../types";
 
 export default function SearchBar() {
   const [searchValue, setSearchValue] = useState("");
-  const [dataProduct, setDataProdutc] = useState([]);
 
-  const { data, loading, refetch } = useQuery(SEARCH_PRODUCTS, {
-    variables: { name: searchValue },
-  });
+  const [searchProduct, { data, loading }] = useLazyQuery(SEARCH_PRODUCTS);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  };
-  const handleQuery = () => {
-    if (searchValue) {
-      setDataProdutc(data["searchProducts"]);
-    }
   };
 
   const handleChange = (e) => {
     e.preventDefault();
     setSearchValue(e.target.value);
-    handleQuery();
+    searchProduct({
+      variables: {
+        name: e.target.value,
+      },
+    });
   };
 
   return (
@@ -38,8 +34,8 @@ export default function SearchBar() {
         <input type="submit" value="submit"></input>
       </form>
       <div>
-        {dataProduct
-          ? dataProduct
+        {searchValue && data && data["searchProducts"]
+          ? data["searchProducts"]
               .sort((a, b) => {
                 var nameA = a.name.toUpperCase();
                 var nameB = b.name.toUpperCase();
