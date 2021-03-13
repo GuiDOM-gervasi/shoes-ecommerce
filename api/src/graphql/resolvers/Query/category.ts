57da0720fe8d3a9f09e47f9bd424f19f37eb8006
Guido <guidogervasi11@gmail.com>
2021-03-13 12:40:00 -0300

    productDetail

--- api/src/graphql/resolvers/Query/category.ts
+++
@@ -1,7 +0,0 @@
-import Category from '../../../db/models/category';
-
-const oneCategoryResolver = async(name) => {
-    const product = await Category.findByPk(name);
-  }
-
-export default oneCategoryResolver
