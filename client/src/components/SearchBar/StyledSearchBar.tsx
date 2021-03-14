b1af5017a15718a1b9d5e367edfe06842a9e9deb
Ayala Aguirre Lindolfo Maximiliano <lindolfomaximiliano@gmail.com>
2021-03-14 16:16:25 -0300

    fix styled searchbar

--- client/src/components/SearchBar/StyledSearchBar.tsx
+++ client/src/components/SearchBar/StyledSearchBar.tsx
@@ -1,3 +1,84 @@
 import styled from 'styled-components'
 
-export const StyledSearchBar = styled.div``
+export const StyledSearchBar = styled.div`
+  width: 100%;
+  max-width: 20rem;
+  padding: 0.2rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+
+  .formSearch {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
+
+    input{
+    border: 1px solid transparent;
+    background-color: #f1f1f1;
+    padding: 0.5rem 0;
+    }
+
+    input[type=text] {
+      background-color: #f1f1f1;
+      width: 80%;
+    }
+
+    input[type=submit] {
+      background-color: #f1f1f1;
+      width:20%;
+      color: #6930C3;
+    }
+
+  }
+
+  .contentResult{
+    width:100%;
+    max-width: 20rem;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    height: min-content;
+    top: 2.5rem;
+  }
+
+  .contentResultItem{
+    position: relative;
+    border: 1px solid #c4c4c4;
+    z-index: 99;
+    top: 100%;
+    left: 0;
+    right: 0;
+
+    div {
+    padding: 0.3rem 0.5rem;
+    cursor: pointer;
+    background-color: #fff;
+    display: flex;
+    
+      &:hover{
+        color: #6930C3;
+      }
+      .name{
+        width: 100%;
+        color: #000;
+      }
+
+      .goPage{
+        background-color: #6930C3;
+        border: 0.1rem solid #6930C3;
+        color:#fff;
+        width: 100%;
+        &:hover{
+          background-color: #fff;
+          border: 0.1rem solid #6930C3;
+          color: #6930C3;
+        }
+      }
+
+    }
+
+  }
+`;
--- client/src/components/SearchBar/index.tsx
+++ client/src/components/SearchBar/index.tsx
@@ -1,55 +1,76 @@
 import React, { useState } from "react";
 import { StyledSearchBar } from "./StyledSearchBar";
-import { SEARCH_PRODUCTS, GET_PRODUCTS } from "../../graphql/queries";
+import { SEARCH_PRODUCTS } from "../../graphql/queries";
 import { useLazyQuery } from "@apollo/react-hooks";
-import { QueryProducts } from "./../../types";
+import { Link, useHistory } from 'react-router-dom'
+
 
 export default function SearchBar() {
+  
   const [searchValue, setSearchValue] = useState("");
+  const [activeAutoComplete, setActiveAutoComplete] = useState(false);
+  const [searchProduct, { called, loading, data }] = useLazyQuery(SEARCH_PRODUCTS);
+  const history = useHistory();
 
-  const [searchProduct, { data, loading }] = useLazyQuery(SEARCH_PRODUCTS);
-
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
   };
 
+  const handleClick = (e) => {
+    const query = e.target.innerText;
+    setSearchValue(query);
+    history.push(`/search?query=${query}`);
+  }
+
   const handleChange = (e) => {
-    e.preventDefault();
-    setSearchValue(e.target.value);
+    const query = e.target.value;
+    setSearchValue(query);
+    query && setActiveAutoComplete(!activeAutoComplete);
     searchProduct({
       variables: {
-        name: e.target.value,
+        name: query,
       },
     });
   };
 
   return (
     <StyledSearchBar>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          onChange={handleChange}
-          value={searchValue}
-        ></input>
-        <input type="submit" value="submit"></input>
-      </form>
-      <div>
-        {searchValue && data && data["searchProducts"]
-          ? data["searchProducts"]
-              .sort((a, b) => {
-                var nameA = a.name.toUpperCase();
-                var nameB = b.name.toUpperCase();
-                if (nameA < nameB) {
-                  return -1;
-                }
-                if (nameA > nameB) {
-                  return 1;
-                }
-                return 0;
-              })
-              .map((item, i) => <h5 key={i}>{item.name}</h5>)
-          : null}
-      </div>
+        <form autoComplete="off" onSubmit={handleSubmit} className="formSearch">
+          <input
+            type="text"
+            onChange={handleChange}
+            value={searchValue}
+            placeholder="Zapatilla Nike Airmax..."
+          ></input>
+          <input type="submit" value="search" className="boton"></input>
+        </form>
+        {
+          (called && loading) ? null : 
+          <div className={activeAutoComplete ? "contentResult" : "contentResult"}>
+          {searchValue && data && data["searchProducts"]
+            ? data["searchProducts"]
+                .sort((a: { name: string }, b: { name: string }) => {
+                  var nameA = a.name.toUpperCase();
+                  var nameB = b.name.toUpperCase();
+                  if (nameA < nameB) {
+                    return -1;
+                  }
+                  if (nameA > nameB) {
+                    return 1;
+                  }
+                  return 0;
+                })
+                .map((item, i) => {
+                  if(i<3){
+                    return (<div key={i} className="contentResultItem">
+                    <div>
+                      <div className="name" onClick={handleClick}>{item.name}</div>
+                      <Link to={`/product/${item.id}`}><div className="goPage">go!</div></Link>
+                    </div>
+                  </div>)
+                  }}) : null}
+    </div>
+        }
     </StyledSearchBar>
   );
 }
