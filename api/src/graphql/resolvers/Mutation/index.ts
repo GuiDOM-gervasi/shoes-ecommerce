f8b6fb7c30262d2b11974fb1fdbf6a0964c28d2d
Martin Tozer <martintoz@gmail.com>
2021-03-15 22:32:04 +0100

    delete y undelete categories

--- api/src/graphql/resolvers/Mutation/index.ts
+++ api/src/graphql/resolvers/Mutation/index.ts
@@ -4,3 +4,5 @@ export {default as createCategory} from "./createCategory";
 export {default as createBrand} from "./createBrand";
 export {default as createModel} from './createModel';
 export {default as updateProduct} from "./updateProduct"
+export {default as deleteCategory} from './deleteCategory';
+export {default as undeleteCategory} from './undeleteCategory';
