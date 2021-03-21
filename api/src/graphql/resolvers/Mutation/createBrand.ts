2789823a22926fecbf43b569ac747cc569efb78a
Maximiliano Ayala <lindolfomaximiliano@gmail.com>
2021-03-21 14:15:41 -0300

    Delete auth for create brand

--- api/src/graphql/resolvers/Mutation/createBrand.ts
+++ api/src/graphql/resolvers/Mutation/createBrand.ts
@@ -2,14 +2,7 @@ import Brand from "../../../db/models/brands";
 import {BrandAttributes} from '../../../db/models/types'
 
 const createBrand = async (parent: any,{name}: BrandAttributes, context: any , info: any) =>{
-  if(context.req.userId){
     return await Brand.create({name});
-  }else{
-    return {
-      name: "Sin token",
-      id:2
-    }
-  }
 }
 
 export default createBrand
