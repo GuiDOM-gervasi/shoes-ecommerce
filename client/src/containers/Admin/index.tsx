37ee42639966da4a327e1f3d78764788babbca73
julignacio <ignaciojulianramirez@gmail.com>
2021-03-22 00:37:40 -0300

    fix errors and adds Admin route

---
+++ client/src/containers/Admin/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { StyledAdmin } from "./StyledAdmin";
+
+export default function Admin() {
+  return (
+    <StyledAdmin>
+      <div>
+        <Link to="/admin/products">Products </Link>
+        <Link to="/admin/addProduct"> Add product </Link>
+      </div>
+      <div>
+        <Link to="/admin/category"> Categories </Link>
+        <Link to="/admin/addCategory"> Add category </Link>
+      </div>
+    </StyledAdmin>
+  )
+}
