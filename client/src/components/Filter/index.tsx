26ead8b573d9505800580576b865cb162f2367f9
Martin Tozer <martintoz@gmail.com>
2021-03-23 22:59:29 +0100

    filter con estilos

--- client/src/components/Filter/index.tsx
+++ client/src/components/Filter/index.tsx
@@ -1,57 +1,69 @@
-import React from 'react'
+import React from "react";
+import { StyledFilter } from "./StyledFilter";
 import { useQuery, useLazyQuery } from "@apollo/client";
-import { GET_CATEGORIES, GET_PRODUCTS_BY_CATEGORIES } from "../../graphql/queries";
+import {
+  GET_CATEGORIES,
+  GET_PRODUCTS_BY_CATEGORIES,
+} from "../../graphql/queries";
 
 // typescript interface
 interface Category {
-  id: string
-  name: string
-  __typename: string
-
+  id: string;
+  name: string;
+  __typename: string;
 }
 
-export default function Filter({setLoadedProduct}) {
-
+export default function Filter({ setLoadedProduct }) {
   // get form apollo/graphQL the categories to be show as options to filter
   const { data, loading, error } = useQuery(GET_CATEGORIES, {});
-  const [getProducts, { data:productsData, loading:productsLoading }] = useLazyQuery(GET_PRODUCTS_BY_CATEGORIES);
+  const [
+    getProducts,
+    { data: productsData, loading: productsLoading },
+  ] = useLazyQuery(GET_PRODUCTS_BY_CATEGORIES);
 
   const handleCategoryFilter = (value) => {
-    console.log(value)
+    console.log(value);
     getProducts({
       variables: {
         name: value,
       },
     });
-  }
-  
+  };
 
   if (loading) {
-    return <div>Loading ...</div>
+    return <div>Loading ...</div>;
   }
   if (error) {
-    return <div>something go wrong loading the filter bar</div>
+    return <div>something go wrong loading the filter bar</div>;
   }
-  console.log('filter component')
-  console.log(productsLoading)
-  console.log(productsData)
-  if(!productsLoading && !!productsData) {
-    setLoadedProduct(productsData.productForCategory)
+  console.log("filter component");
+  console.log(productsLoading);
+  console.log(productsData);
+  if (!productsLoading && !!productsData) {
+    setLoadedProduct(productsData.productForCategory);
   }
 
   const categories: Category[] = data.categories;
 
   return (
-    <div>
-      Filter{" "}
-      <select onChange={(
-                ev: React.ChangeEvent<HTMLSelectElement>,
-            ): void => handleCategoryFilter(ev.target.value)}>
-          <option id="zzz" value = ''>todas</option>
-        {categories.map((e) =>
-          <option id={e.id} value={e.name}>{e.name} </option>)
-        }
-      </select>
-    </div>
+    <StyledFilter>
+      <div className="filter">
+        <select
+          onChange={(ev: React.ChangeEvent<HTMLSelectElement>): void =>
+            handleCategoryFilter(ev.target.value)
+          }
+        >
+          <option className="option" id="zzz" value="">
+            Todas
+          </option>
+          {categories.map((e) => (
+            <option className="option" id={e.id} value={e.name}>
+              {e.name}{" "}
+            </option>
+          ))}
+        </select>
+        <span className="focus"></span>
+      </div>
+    </StyledFilter>
   );
 }
