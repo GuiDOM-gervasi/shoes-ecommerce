bac1086b28f4c9e47c5bab13833c24e9beb3809a
julignacio <ignaciojulianramirez@gmail.com>
2021-03-24 14:12:41 -0300

    fix quantity and total error

--- client/src/containers/Cart/StyledCart.tsx
+++ client/src/containers/Cart/StyledCart.tsx
@@ -1,103 +1,101 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const StyledCart = styled.div`
-.container{
+  .container {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
     margin-bottom: 9rem;
-    div{     
-        margin: 3rem 5rem;
+    div {
+      margin: 3rem 5rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      position: relative;
+      .buttonDelete {
+        position: absolute;
+        top: 0.2rem;
+        right: 0.5rem;
+        width: 1.5rem;
+        background-color: #f11717;
+        border: 0;
+        border-radius: 5px;
+        color: white;
+        cursor: pointer;
+        &:focus {
+          outline: none;
+        }
+        &:hover {
+          background-color: #c02020;
+        }
+      }
+      img {
+        width: 15rem;
+      }
+
+      input[type="number"] {
+        appearance: textfield;
+      }
+
+      input[type="number"]::-webkit-inner-spin-button,
+      input[type="number"]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+      }
+
+      .number-input {
+        border: 2px solid #ddd;
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
+      }
+
+      .number-input,
+      .number-input * {
+        box-sizing: border-box;
+      }
+
+      .number-input button {
+        outline: none;
+        -webkit-appearance: none;
+        background-color: transparent;
+        border: none;
         align-items: center;
+        justify-content: center;
+        width: 3rem;
+        height: 3rem;
+        cursor: pointer;
+        margin: 0;
         position: relative;
-            .buttonDelete{
-                position: absolute;
-                top: 0.2rem;
-                right: 0.5rem;
-                width:1.5rem;
-                background-color:#f11717;
-                border:0;
-                border-radius:5px;
-                color:white;
-                cursor: pointer;
-                &:focus{
-                     outline: none;
-                }
-                &:hover{
-                    background-color: #c02020;
+      }
 
-                }
-            }
-            img{
-                width: 15rem;
-            }
+      .number-input button:before,
+      .number-input button:after {
+        display: inline-block;
+        position: absolute;
+        content: "";
+        width: 0.8rem;
+        height: 2px;
+        background-color: #212121;
+        transform: translate(-50%, -50%);
+      }
+      .number-input button.plus:after {
+        transform: translate(-50%, -50%) rotate(90deg);
+      }
 
-            input[type=number] {
-            appearance: textfield;
-            }
+      .number-input input[type="number"] {
+        font-family: sans-serif;
+        max-width: 3rem;
+        padding: 0.5rem;
+        border: solid #ddd;
+        border-width: 0 2px;
+        font-size: 1.5rem;
+        height: 2rem;
+        font-weight: bold;
+        text-align: center;
+      }
+    }
+  }
 
-            input[type=number]::-webkit-inner-spin-button,
-            input[type=number]::-webkit-outer-spin-button {
-            -webkit-appearance: none;
-            }
-
-            .number-input {
-            border: 2px solid #ddd;
-            display: inline-flex;
-            }
-
-            .number-input,
-            .number-input * {
-            box-sizing: border-box;
-            }
-
-            .number-input button {
-            outline:none;
-            -webkit-appearance: none;
-            background-color: transparent;
-            border: none;
-            align-items: center;
-            justify-content: center;
-            width: 3rem;
-            height: 3rem;
-            cursor: pointer;
-            margin: 0;
-            position: relative;
-            }
-
-            .number-input button:before,
-            .number-input button:after {
-            display: inline-block;
-            position: absolute;
-            content: '';
-            width: 1rem;
-            height: 2px;
-            background-color: #212121;
-            transform: translate(-50%, -50%);
-            }
-            .number-input button.plus:after {
-            transform: translate(-50%, -50%) rotate(90deg);
-            }
-
-            .number-input input[type=number] {
-            font-family: sans-serif;
-            max-width: 5rem;
-            padding: .5rem;
-            border: solid #ddd;
-            border-width: 0 2px;
-            font-size: 2rem;
-            height: 3rem;
-            font-weight: bold;
-            text-align: center;
-            }
-            
-            }
-
-}
-
-footer{
+  footer {
     display: flex;
     position: fixed;
     bottom: 0;
@@ -109,15 +107,13 @@ footer{
     padding: 2rem;
     height: 9rem;
     justify-content: space-between;
-    button{
-        width: 20rem;
-        height: 2.5rem;
-        padding: 0;
-        &:focus{
-            outline: none;
-        }
+    button {
+      width: 20rem;
+      height: 2.5rem;
+      padding: 0;
+      &:focus {
+        outline: none;
+      }
     }
-}
-
-    
+  }
 `;
--- client/src/containers/Cart/index.tsx
+++ client/src/containers/Cart/index.tsx
@@ -1,116 +1,136 @@
-import React from 'react'
-import { GET_CART } from '../../graphql/queries'
-import { StyledCart } from './StyledCart'
-import {useMutation, useQuery} from "@apollo/client"
-import { CartAttributes } from '../../types'
-import { fotosZapa } from '../../components/ProductDetail/mockup'
-import { useAuth } from '../../hooks/AuthProvider'
-import { DELETE_TO_CART, QUANTITY } from '../../graphql/mutations'
-import Loader from '../../components/Loader';
-import { LocalPersistence } from '../../helpers/localPersistence';
+import React from "react";
+import { GET_CART } from "../../graphql/queries";
+import { StyledCart } from "./StyledCart";
+import { useMutation, useQuery } from "@apollo/client";
+import { CartAttributes } from "../../types";
+import { fotosZapa } from "../../components/ProductDetail/mockup";
+import { useAuth } from "../../hooks/AuthProvider";
+import { DELETE_TO_CART, QUANTITY } from "../../graphql/mutations";
+import Loader from "../../components/Loader";
+import { LocalPersistence } from "../../helpers/localPersistence";
 
 const Cart = () => {
-const {userId} = useAuth()
-const {data, loading, error} = useQuery(GET_CART, {
-        variables: {
-          userId: userId&&userId
-        },})
+  const { userId } = useAuth();
+  const { data, loading, error } = useQuery(GET_CART, {
+    variables: {
+      userId: userId && userId,
+    },
+  });
 
-const [deleteProductCart, { loading: loadingDelete }] = useMutation(
+  const [deleteProductCart, { loading: loadingDelete }] = useMutation(
     DELETE_TO_CART,
+    {
+      refetchQueries: [
         {
-            refetchQueries: [{ query: GET_CART, variables:{
-             userId: userId&&userId   
-            }}],
-        }
-);
+          query: GET_CART,
+          variables: {
+            userId: userId && userId,
+          },
+        },
+      ],
+    }
+  );
 
-const [controlQuantity, {loading: loadingQuantity}] = useMutation(
+  const [controlQuantity, { loading: loadingQuantity }] = useMutation(
     QUANTITY,
     {
-        refetchQueries: [{ query: GET_CART, variables:{
-            userId: userId&&userId   
-           }}],
+      refetchQueries: [
+        {
+          query: GET_CART,
+          variables: {
+            userId: userId && userId,
+          },
+        },
+      ],
     }
-)
+  );
 
-if (loading) return <Loader />;
-if (error) return <span>Error {error.message}</span>;
-const products= data.cart?.finalproducts
-console.log(products)
+  if (loading) return <Loader />;
+  if (error) return <span>Error {error.message}</span>;
+  const products = data.cart?.finalproducts;
+  console.log(products);
 
-const {
-    photo,
-  } = fotosZapa;
-let count = 0;
+  const { photo } = fotosZapa;
+  let count = 0;
 
-const handleDelete = (finalproduct) => {
-    
+  const handleDelete = (finalproduct) => {
     deleteProductCart({
-        variables:{
-            cartId: data.cart?.id,
-            finalproductId: finalproduct
-        } 
-    })
-}
+      variables: {
+        cartId: data.cart?.id,
+        finalproductId: finalproduct,
+      },
+    });
+  };
 
-
-const handleQuantity = (e,id) => {
+  const handleQuantity = (e, id) => {
     controlQuantity({
-        variables:{
-            id,
-            quantity: parseInt(e.target.value)
-        }
-    })    
-}
+      variables: {
+        id,
+        quantity: parseInt(e.target.value),
+      },
+    });
+  };
 
-const handleClick = (e) => {
-    const input : any = e.target.parentNode.querySelector("#quantity")
-    if(e.target.id === "mas"){
-       return input.value ++
+  const handleClick = (e, p) => {
+    const input: any = e.target.parentNode.querySelector("#quantity");
+    if (e.target.id === "mas") {
+      return handleQuantity(
+        { target: { value: Number(input.value) + 1 } },
+        p.cartproducts[0].id
+      );
     }
-    if(input.value > 1){
-        input.value --
+    if (input.value > 1) {
+      return handleQuantity(
+        { target: { value: Number(input.value) - 1 } },
+        p.cartproducts[0].id
+      );
     }
-}
-    return (
-       <StyledCart className="fondoDegradado">
-           <div className="container ">
-           {
-               
-               products?.map((p)=>{
-                count += p.product.price * p.cartproducts[0].quantity
-                return (
-                    <div>
-                        <img
-                            src={p.product.muestraimg}
-                            alt={`photoDetail 3 - ${p.product.name}`}
-                        />            
-                        <h4>{p.product.name}</h4> 
-                        <p>Price: {p.product.price}</p>
-                        <button className="buttonDelete" onClick={()=>handleDelete(p.id)}>X</button>
-                        <div className="number-input">
-                        <button id="-" onClick={handleClick} ></button>
-                        <input 
-                        id="quantity"
-                        className="quantity"
-                        type="number" 
-                        onChange={(e)=>handleQuantity(e,p.cartproducts[0].id)}
-                        value={p.cartproducts[0].quantity}
-                        /> 
-                        <button id="mas" onClick={handleClick} className="plus"></button>
-                        </div>    
-                    </div>
-                )
-               })
-            }
+  };
+
+  return (
+    <StyledCart className="fondoDegradado">
+      <div className="container ">
+        {products?.map((p) => {
+          count += p.product.price * p.cartproducts[0].quantity;
+          return (
+            <div>
+              <img
+                src={p.product.muestraimg}
+                alt={`photoDetail 3 - ${p.product.name}`}
+              />
+              <h4>{p.product.name}</h4>
+              <p>Price: {p.product.price}</p>
+              <button
+                className="buttonDelete"
+                onClick={() => handleDelete(p.id)}
+              >
+                X
+              </button>
+              <div className="number-input">
+                <button id="-" onClick={(e) => handleClick(e, p)}></button>
+                <input
+                  id="quantity"
+                  className="quantity"
+                  type="number"
+                  onChange={(e) => handleQuantity(e, p.cartproducts[0].id)}
+                  value={p.cartproducts[0].quantity}
+                />
+                <button
+                  id="mas"
+                  onClick={(e) => handleClick(e, p)}
+                  className="plus"
+                ></button>
+              </div>
             </div>
-            <footer>
-                <h5>Total: {count}</h5>
-                <button className="boton">Buy</button>
-            </footer>
-       </StyledCart>
-    )
-}
+          );
+        })}
+      </div>
+      <footer>
+        <h5>Total: {count}</h5>
+        <button className="boton">Buy</button>
+      </footer>
+    </StyledCart>
+  );
+};
 
-export default Cart
+export default Cart;
