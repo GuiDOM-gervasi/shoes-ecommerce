6db138ca8ff6c0bb3088e6fc14c495f5aadd512b
Martin Tozer <martintoz@gmail.com>
2021-03-25 01:33:04 +0100

    seeds stock to finalproducts

--- api/sequelize/seeders/zproductmodels.ts
+++ api/sequelize/seeders/zproductmodels.ts
@@ -3,94 +3,112 @@ module.exports = {
     return queryInterface.bulkInsert('finalproducts', [{
 						productId: "1",
 						modelId:"1",
+						stock:"1",
       createdAt: new Date(),
       updatedAt: new Date()
     },{
 						productId:"1",
 						modelId:"2",
+						stock:"10",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"7",
 						modelId:"3",
+						stock:"15",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"2",
 						modelId:"3",
+						stock:"5",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"2",
 						modelId:"5",
+						stock:"0",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"8",
 						modelId:"1",
+						stock:"0",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"3",
 						modelId:"4",
+						stock:"0",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"3",
 						modelId:"3",
+						stock:"24",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"9",
 						modelId:"6",
+						stock:"27",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"4",
 						modelId:"1",
+						stock:"1",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"4",
 						modelId:"6",
+						stock:"3",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"10",
 						modelId:"3",
+						stock:"33",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"5",
 						modelId:"5",
+						stock:"26",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"5",
 						modelId:"6",
+						stock:"42",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"11",
 						modelId:"2",
+						stock:"2",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"6",
 						modelId:"4",
+						stock:"0",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"6",
 						modelId:"5",
+						stock:"31",
 						createdAt:new Date(),
 						updatedAt:new Date()
 				},{
 						productId:"12",
 						modelId:"5",
+						stock:"16",
 						createdAt:new Date(),
 						updatedAt:new Date()
-				},]);
+				},]); 
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('finalproducts', null, {});
