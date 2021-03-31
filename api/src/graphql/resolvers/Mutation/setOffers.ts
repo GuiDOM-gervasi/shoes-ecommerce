7f9d97f14a17d442a16664c9703abaa4e11cb8c3
mstefa <mstefanutti@gmail.com>
2021-03-31 17:24:19 -0300

    mutation for set offer by category

--- api/src/graphql/resolvers/Mutation/setOffers.ts
+++ api/src/graphql/resolvers/Mutation/setOffers.ts
@@ -4,22 +4,32 @@ import { handlerUpdate } from "#root/helpers/updateProduct";
 import Product from "../../../db/models/products";
 import FinalProduct from "#root/db/models/finalproduct";
 import {UpdateProductAttributes} from '../../../db/models/types';
+import { any } from "sequelize/types/lib/operators";
 
 
-const setOffers = async ( parent:any, args: UpdateProductAttributes )  => {
-    const {categoryId, discount} = args
+const setOffers = async ( parent:any, args: any )  => {
+    let {categoryId, discount} = args
     
-    const prod = await Product.findAll(
+    let prod = await Product.findAll(
       {
         include: {
-          Category as any,
+          model: Category as any,
           where: {
             id: categoryId
           }
+          
         }
       }
     )
-    
+      
+    let update = await Product.update(
+      {discount},
+      {where: 
+        { id: [2, 4, 8]} 
+      }
+    ) 
+    // console.log(`prod`, prod)
+    console.log(`update`, update)
     return "todo Ok"
 }
 
