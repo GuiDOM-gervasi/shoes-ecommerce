69876105033de3ef8c540c0d219ede01ba86b7dd
Guido <guidogervasi11@gmail.com>
2021-03-31 21:36:27 -0300

    fixed /admin && noSearchResults

--- client/src/containers/Admin/StyledAdmin.tsx
+++ client/src/containers/Admin/StyledAdmin.tsx
@@ -1,29 +1,39 @@
 import styled from "styled-components";
-import { violeta } from "../App/GlobalStyles";
+import { violeta, verdeDetalleTrans, blanco } from "../App/GlobalStyles";
 
 export const StyledAdmin = styled.div`
   display: flex;
-  flex-direction: column;
-
+  flex-wrap: wrap;
+  justify-content:center;
+  background-color: ${violeta};
+  height:100%;
   div {
-    width: 100%;
-    height: 3rem;
-    padding: 4rem;
-    background-color: ${violeta}
-    margin: 0 auto;
+    width: 20%;
+    height: 10rem;
+    border-radius: 10px;
+    border: solid ${blanco};
+    background-color:${verdeDetalleTrans};
+    margin: 5rem;
     display: flex;
-    justify-content: space-around;
-
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
     a {
-      color: #f0f0f0;
+      color: ${blanco};
       text-decoration: none;
       font-weight: 750;
       font-size: 1.5rem;
       transition: transform 500ms ease;
-      
+      margin-bottom: 1rem;
       &:hover {
         transform: scale(1.1)
       }
     }
-  }  
+  }
+
+  @media (max-width: 902px){
+    div{
+      width:35%;
+    }
+  }
 `;
