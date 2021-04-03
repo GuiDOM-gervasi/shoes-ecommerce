f13c57ddd3b3f4119448a756e34e4742c7f576ba
mstefa <mstefanutti@gmail.com>
2021-04-03 18:20:35 -0300

    adding offert filter

--- client/src/containers/Catalogue/index.tsx
+++ client/src/containers/Catalogue/index.tsx
@@ -13,6 +13,7 @@ export default function Catalogue() {
   let { data, loading, error } = useQuery(GET_PRODUCTS);
   const [createCart] = useMutation(CREATE_CART);
   const [loadedProducts, setLoadedProduct] = React.useState([]);
+  const [filterOfferts, setFilterOfferts] = React.useState(false);
   const { userId } = useAuth();
   if (parseInt(userId) > 0) {
     createCart({
@@ -34,23 +35,30 @@ export default function Catalogue() {
 
   if (loading || !data) return <Loader />;
   if (error) return <span>Error {error.message}</span>;
-
+  // let filterOfferts = true;
   return (
     <StyledCatalogue className="fondoDegradado">
       <Slider />
       <div className="sectionBar">
-        <section className="create">Crear</section>
-        <section className="trend">Tendencias</section>
-        <section className="sale">Ofertas</section>
+        {/* <section className="create">Crear</section>
+        <section className="trend">Tendencias</section> */}
+        <section className="sale" onClick={() => setFilterOfferts(!filterOfferts)}>For sale!</section>
         <Filter setLoadedProduct={setLoadedProduct} />
       </div>
-
+      
       <ul>
-        {loadedProducts.map((item, i) => (
-          <li key={item.id}>
-            <ProductCard item={item} />
-          </li>
-        ))}
+        {loadedProducts.map((item, i) => {
+          
+            if(!filterOfferts || item.discount > 0 ){
+              return (            
+              <li key={item.id}>
+                <ProductCard item={item} />
+              </li>
+            )}
+              else{
+                return <></>
+              }
+            })}
       </ul>
     </StyledCatalogue>
   );
