7313e9654cb2864ffc34fe2ebf84a482ddf04a39
mstefa <mstefanutti24@gmail.com>
2021-04-04 13:47:16 -0300

    Styling fav button and make it resposive

--- client/src/components/ProductDetail/StyledProductDetail.tsx
+++ client/src/components/ProductDetail/StyledProductDetail.tsx
@@ -3,7 +3,6 @@ import {
   blanco,
   verdeMain,
   violeta,
-  productWidth,
   verdeDetalle,
 } from "../../containers/App/GlobalStyles";
 
@@ -131,6 +130,23 @@ export const StyledProductDetail = styled.div`
         cursor: pointer;
       }
     }
+
+    .favoritos{
+      grid-row: 4 ;
+      grid-column: 3;
+      .fas{
+        font-size: 3rem;
+      }
+      .fa-heart.fas-on{
+        color: ${violeta};
+      }
+
+      .fa-heart.fas-off{
+        color: ${blanco};
+        text-shadow: -1px 0 #2b2b2b, 0 1px black, 1px 0 black, 0 -1px black;
+        
+      }
+    }
   }
   .related {
     margin-top: 1rem;
@@ -211,6 +227,8 @@ export const StyledProductDetail = styled.div`
     padding: 0rem 0.5rem 0rem 0.5rem;
   }
 
+  
+
   @media screen and (max-width: 870px) {
     .container {
       grid-template-columns: 1fr;
@@ -219,6 +237,8 @@ export const StyledProductDetail = styled.div`
     .fondoVioleta {
       display: none;
     }
+
+
 
     .imagenes {
       padding-left: 0;
@@ -285,6 +305,12 @@ export const StyledProductDetail = styled.div`
         background-color: ${violeta};
         width: 100%;
       }
+      .favoritos{
+        grid-row:6;
+        grid-column: 2/3;
+        justify-self:center;
+      }
+
       h1,
       .tituloLargo,
       .tituloCorto {
--- client/src/components/ProductDetail/index.tsx
+++ client/src/components/ProductDetail/index.tsx
@@ -14,14 +14,17 @@ import {
 import { fotosZapa } from "./mockup";
 import { Link } from "react-router-dom";
 import Loader from "../Loader";
-import { ADD_TO_CART, ADD_TO_WISHLIST } from "../../graphql/mutations";
+import { ADD_TO_CART, ADD_TO_WISHLIST, DELETE_FROM_WISHLIST } from "../../graphql/mutations";
 import { useAuth } from "../../hooks/AuthProvider";
 import Reviews from "../../containers/Reviews";
 import Swal from "sweetalert2";
-import fav from "./heart/fav.png"
+// import fav from "./heart/fav.png"
+
+
 export default function ProductDetail({ match }: any) {
   const productId = match.params.id;
   const { userId } = useAuth();
+  const [isFavorite, setFavorite] = React.useState(false);
 
   const [addToCart] = useMutation(ADD_TO_CART, {
     refetchQueries: [
@@ -51,11 +54,38 @@ export default function ProductDetail({ match }: any) {
     ]
   })
 
+  const [removeFromWishlist] = useMutation(DELETE_FROM_WISHLIST,{
+    refetchQueries:[
+      {
+        query: GET_WISHLIST,
+        variables:{
+          userId: userId && userId
+        }
+      }
+    ]
+  })
+
+  useQuery(GET_WISHLIST, {
+    variables: {
+      userId: userId && userId,
+    },
+    onCompleted: wishData =>{
+      wishData?.wishList.forEach(product => {
+        if (product.product.id === productId){
+          setFavorite(true);
+          return true
+        }
+        return false
+      })
+    }
+  });
+
   const { data } = useQuery(GET_CART_SIMPLE, {
     variables: {
       userId: userId && userId,
     },
   });
+
 
   const { loading, error, data: mainProduct } = useQuery(GET_PRODUCT_DETAIL, {
     variables: {
@@ -75,8 +105,6 @@ export default function ProductDetail({ match }: any) {
 
   const {
     data: reviewData,
-    loading: reviewLoading,
-    error: reviewError,
   } = useQuery(GET_REVIEWS, {
     variables: {
       productId,
@@ -235,19 +263,32 @@ export default function ProductDetail({ match }: any) {
   };
 
   const addToFav = (productId) =>{
-     Swal.fire({
-          icon: "success",
-          title: "Great choice!",
-          text: "Product successfully added to your wishlist",
-          showConfirmButton: false,
-          timer: 1500,
-        });
+    // Swal.fire({
+    //       icon: "success",
+    //       title: "Great choice!",
+    //       text: "Product successfully added to your wishlist",
+    //       showConfirmButton: false,
+    //       timer: 1500,
+    //     });
+    
     addToWishlist({
       variables:{
         productId,
         userId
       }
     })
+    setFavorite(true)
+    console.log('agregar')
+  }
+
+  const removeFromFav = (productId) =>{
+    removeFromWishlist({
+      variables:{
+        productId,
+        userId
+      }
+    })
+    setFavorite(false)
   }
 
   return (
@@ -320,8 +361,12 @@ export default function ProductDetail({ match }: any) {
               <></>
             )}
           </div>
+          <div className="favoritos">
+              { isFavorite
+                ? <i className="fas fa-heart fas-on" onClick={()=>removeFromFav(id)}></i> 
+                : <i className="fas fa-heart fas-off" onClick={()=>addToFav(id)}></i>}
+          </div>
           <div className="botones">
-          <i className="fas fa-heart" onClick={()=>addToFav(id)}></i>
             <select
               className="botonInvertido"
               onChange={(e: any) => {
