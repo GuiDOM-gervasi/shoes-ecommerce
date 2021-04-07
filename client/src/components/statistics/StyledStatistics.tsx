cf7f87d10a427f5bd80568f6aad6f2dc88c7f068
shlock5 <delucaivan.01@gmail.com>
2021-04-07 17:01:28 -0300

    charts styles

--- client/src/components/statistics/StyledStatistics.tsx
+++ client/src/components/statistics/StyledStatistics.tsx
@@ -1,92 +1,51 @@
 import styled from "styled-components";
-import { blanco, violeta } from "../../containers/App/GlobalStyles";
+import { blanco, violeta, formWidth } from "../../containers/App/GlobalStyles";
 
 export const StyledStatistics = styled.div`
+  
+  .crud_container{
   display: flex;
-  flex-direction: column;
+  /* flex-direction: column; */
+  justify-self: center;
+  justify-content: space-around;
+  /* width: ${formWidth * 2}vw; */
+  min-height: 30vh;
+  margin: 2rem 1.5rem 0;
+  padding: 1rem;
+  border: 2px solid black;
+  border-radius: 15px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+  background-color: ${blanco};/* ;ex-direction: column; */
+  }
+  
+  .barContainer, .donutContainer{
+    width: 50vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    canvas{
+      height: 50vh !important;
+      width: 100%;
+    }
+  }
+
   h3{
     text-align:center;
     text-transform:capitalize;
-    padding:1rem;
+    padding:1rem 1.5rem;
+    width: 50%;
+    border-radius: 15px;
     color:${blanco};
     background-color:${violeta};
   }
-  .sectionBar section{
-    text-transform:capitalize;
-  }
-  ul li {
-    width: 100%;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    margin: 1rem 0;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-
-    span,
-    select {
-      width: 15%;
-      display: flex;
-      justify-content: center;
-      text-align: center;
-    }
-
-    button {
-      margin: 1rem 0;
-      padding: 0.5rem;
-      border-radius: 5px;
-      border: 1px solid rgba(0, 0, 0, 0.3);
-      box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
-      align-self: center;
-    }
-  }
-
-  .titles * {
-    font-weight: 1000;
-    font-size: 1.2em;
-  }
-
-  ul li + li {
-    .product {
-      cursor: pointer;
-
-      &:hover {
-        transform: scale(1.1);
+  
+  @media (max-width: 858px){
+    .crud_container{
+      flex-direction: column !important;
+      .donutContainer, .barContainer{
+        width: 100vw;
       }
     }
   }
-  p.product,
-  p.model,
-  p.quantity,
-  p.price,
-  p.state,
-  p.username {
-    display: none;
-  }
-  @media (max-width: 858px) {
-    ul li {
-      width:90vw;
-      margin:3vh auto;
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      grid-gap:5vw;
-      grid-template-rows:1fr 1fr 1fr;
-      span, select{
-        width:100%;
-        flex-direction:column;
-        align-items:flex-start;
-      }
-    }
-    .titles {
-      display: none;
-    }
-    p.product,
-    p.model,
-    p.quantity,
-    p.price,
-    p.state,
-    p.username {
-      display: inline;
-      font-weight:bold;
-    }
-  }
+
 `;
