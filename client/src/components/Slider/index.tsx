76d6f162a8ee187c6d339de24e7841b5e8cfcb32
Martin Tozer <martintoz@gmail.com>
2021-04-08 16:03:56 +0200

    styled stats

--- client/src/components/Slider/index.tsx
+++ client/src/components/Slider/index.tsx
@@ -9,27 +9,39 @@ export default function Slider() {
 
   return (
     <StyledSlider>
-      <Carousel showThumbs={false} autoPlay={true} infiniteLoop={true} interval={5000}>
+      <Carousel
+        showThumbs={false}
+        autoPlay={true}
+        infiniteLoop={true}
+        interval={5000}
+      >
         <NavLink to="/product/1">
           <div>
-            <img src="https://user-images.githubusercontent.com/74660801/113933544-02853c80-97f5-11eb-95f1-c8d2f7895e34.jpg" alt='slider1'/>
+            <img
+              src="https://user-images.githubusercontent.com/74660801/113933544-02853c80-97f5-11eb-95f1-c8d2f7895e34.jpg"
+              alt="slider1"
+            />
           </div>
         </NavLink>
-        
+
         <NavLink to="/product/2">
           <div>
-            <img src="https://user-images.githubusercontent.com/74660801/113934044-93f4ae80-97f5-11eb-9769-eb7987e1aa0c.jpg"  alt='slider2' />
-            </div>
+            <img
+              src="https://user-images.githubusercontent.com/74660801/113934044-93f4ae80-97f5-11eb-9769-eb7987e1aa0c.jpg"
+              alt="slider2"
+            />
+          </div>
         </NavLink>
-        
-        <div>
-          <NavLink to="/product/3">
-            <img src="https://user-images.githubusercontent.com/74660801/113934025-8f2ffa80-97f5-11eb-9e5a-d9bf3c39b9ea.jpg" alt='slider3'/>
-          </NavLink>
-        </div>
 
+        <NavLink to="/product/3">
+          <div>
+            <img
+              src="https://user-images.githubusercontent.com/74660801/113934025-8f2ffa80-97f5-11eb-9e5a-d9bf3c39b9ea.jpg"
+              alt="slider3"
+            />
+          </div>
+        </NavLink>
       </Carousel>
-
     </StyledSlider>
   );
 }
--- client/src/components/Statistics/StyledStatistics.tsx
+++ client/src/components/Statistics/StyledStatistics.tsx
@@ -14,40 +14,33 @@ export const StyledStatistics = styled.div`
     box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
     background-color: ${blanco}; /* ;ex-direction: column; */
   }
-
-  .barContainer,
-  .donutContainer {
+  .containerStatistics {
     width: 45vw;
     display: flex;
     flex-direction: column;
     align-items: center;
     canvas {
-      height: 50vh !important;
+      max-height: 50vh !important;
       width: 100%;
     }
   }
-
   h3 {
     text-align: center;
     text-transform: capitalize;
     padding: 1rem 1.5rem;
-    width: 50%;
+    width: 100%;
     border-radius: 15px;
     color: ${blanco};
     background-color: ${violeta};
   }
-
   @media (max-width: 858px) {
     .crud_container {
       flex-direction: column !important;
       align-items: center;
       margin: 2rem auto 0;
-
-      .donutContainer,
-      .barContainer {
+      .containerStatistics {
         width: 90%;
         margin: 2rem 0 0;
-
         canvas {
           width: 100% !important;
         }
